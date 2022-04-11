d8c5cbb27dbceac0509b15ab1438a8808949cade
Artem Paliutin <pal.artem@icloud.com>
2022-04-11 11:55:15 +0300

    Initial Commit

---
+++ ShopDemo/ShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDemoApp.swift
+//  ShopDemo
+//
+//  Created by Artem Paliutin on 11/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
