4f0b552bfc4f30a8120618427fff53b628363e18
Artem Paliutin <pal.artem@icloud.com>
2022-04-12 14:05:38 +0300

    fix

--- ShopDemo/PaymentHandler.swift
+++ ShopDemo/PaymentHandler.swift
@@ -24,6 +24,9 @@ class PaymentHandler: NSObject {
         .mir
     ]
     
+    // MARK: - Functions
+    
+    // MARK: shippingMethodCalculator
     func shippingMethodCalculator() -> [PKShippingMethod] {
         let today = Date()
         let calendar = Calendar.current
@@ -45,4 +48,63 @@ class PaymentHandler: NSObject {
         return []
     }
     
+    // MARK: startPayment
+    func startPayment(products: [Product], total: Int, completion: @escaping PaymentCompletionHandler) {
+        completionHandler = completion
+        
+        paymentSummaryItems = []
+        
+        products.forEach { product in
+            let item = PKPaymentSummaryItem(label: product.name, amount: NSDecimalNumber(string: "\(product.price)"), type: .final)
+            paymentSummaryItems.append(item)
+        }
+        
+        let total = PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "\(total).00"), type: .final)
+        paymentSummaryItems.append(total)
+        
+        let paymentRequest = PKPaymentRequest()
+        paymentRequest.paymentSummaryItems = paymentSummaryItems
+        paymentRequest.merchantIdentifier = "merchant.pal.artem.shopDemoOne"
+        paymentRequest.merchantCapabilities = .capability3DS
+        paymentRequest.currencyCode = "US"
+        paymentRequest.currencyCode = "USD"
+        paymentRequest.supportedNetworks = PaymentHandler.supportedNetworks
+        paymentRequest.shippingType = .delivery
+        paymentRequest.shippingMethods = shippingMethodCalculator()
+        paymentRequest.requiredShippingContactFields = [.name, .postalAddress]
+        
+        paymentController = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
+        paymentController?.delegate = self
+    }
+}
+
+
+// MARK: - extension PaymentHandler
+extension PaymentHandler: PKPaymentAuthorizationControllerDelegate {
+    func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        
+        let errors = [Error]()
+        let status = PKPaymentAuthorizationStatus.success
+        
+        self.paymentStatus = status
+        completion(PKPaymentAuthorizationResult(status: status, errors: errors))
+    }
+    
+    func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
+        controller.dismiss {
+            DispatchQueue.main.async {
+                if self.paymentStatus == .success {
+                    if let completionHandler = self.completionHandler {
+                        completionHandler(true)
+                    }
+                } else {
+                    if self.paymentStatus == .success {
+                        if let completionHandler = self.completionHandler {
+                            completionHandler(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
